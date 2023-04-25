86ba5d73d7e0b10de2240e0e69dc2ea55aa01f4e
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-04-25 03:45:16 +0200

    enhance code

--- Mobile/app/src/main/java/com/example/smartstick/ui/search/SearchFragment.kt
+++ Mobile/app/src/main/java/com/example/smartstick/ui/search/SearchFragment.kt
@@ -1,35 +1,28 @@
 package com.example.smartstick.ui.search
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smartstick.MainActivity
-import com.example.smartstick.R
 import com.example.smartstick.data.User
 import com.example.smartstick.data.base.BaseFragment
 import com.example.smartstick.databinding.FragmentSearchBinding
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
-import com.google.android.material.search.SearchBar
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(),SearchView.OnQueryTextListener {
 
-    private lateinit var adapter: SearchAdapter
+    private lateinit var adapter: FirebaseRecyclerAdapter<User, SearchAdapter.ViewHolder>
     private lateinit var mUserRef: DatabaseReference
-//    private lateinit var options : FirebaseRecyclerAdapter<User,SearchAdapter.ViewHolder>
+    private lateinit var options : FirebaseRecyclerOptions<User>
     override val TAG: String =this ::class.simpleName.toString()
     override fun getViewBinding(): FragmentSearchBinding =
         FragmentSearchBinding.inflate(layoutInflater)
 
     override fun setUp() {
         (activity as MainActivity).showBottomNavigationView()
-        val options = FirebaseRecyclerOptions.Builder<User>()
+        options = FirebaseRecyclerOptions.Builder<User>()
             .setQuery(mUserRef , User::class.java).build()
         adapter = SearchAdapter(options,requireContext())
         binding.recyclerViewSearch.adapter = adapter
