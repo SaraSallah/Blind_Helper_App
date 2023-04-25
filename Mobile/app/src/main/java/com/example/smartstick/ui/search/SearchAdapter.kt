220d65b4211feee53663178d0bc3ee23c52bb875
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-04-25 03:31:12 +0200

    add search adapter and create search fragment and design

--- Mobile/app/src/main/java/com/example/smartstick/ui/search/SearchAdapter.kt
+++ Mobile/app/src/main/java/com/example/smartstick/ui/search/SearchAdapter.kt
@@ -1,4 +1,57 @@
 package com.example.smartstick.ui.search
 
-class SearchAdapter {
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smartstick.R
+import com.example.smartstick.data.User
+import com.example.smartstick.databinding.SingleViewFindFriendBinding
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class SearchAdapter (private val options: FirebaseRecyclerOptions<User>, private val context: Context) :
+    FirebaseRecyclerAdapter<User, SearchAdapter.ViewHolder>(options) {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mUser: FirebaseUser
+
+    open class ViewHolder (private val binding: SingleViewFindFriendBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(model: User) {
+            binding.userEmail.text = model.email
+//            binding.userType.text = model.type
+            binding.imageProfile.setImageResource(R.drawable.defualt)
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = SingleViewFindFriendBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+        }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, model: User) {
+        mAuth = FirebaseAuth.getInstance()
+        mUser = mAuth.currentUser!!
+
+        if (mUser.uid != getRef(position).key) {
+            holder.bind(model)
+            holder.itemView.setOnClickListener {
+//                val intent = Intent(context, SearchFragment::class.java)
+//                intent.putExtra("userKey", getRef(position).key.toString())
+//                context.startActivity(intent)
+            }
+        }
+    }
+
 }
+
+
+
+
--- Mobile/app/src/main/java/com/example/smartstick/ui/search/SearchFragment.kt
+++ Mobile/app/src/main/java/com/example/smartstick/ui/search/SearchFragment.kt
@@ -6,20 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smartstick.MainActivity
 import com.example.smartstick.R
+import com.example.smartstick.data.User
 import com.example.smartstick.data.base.BaseFragment
 import com.example.smartstick.databinding.FragmentSearchBinding
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.material.search.SearchBar
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(),SearchView.OnQueryTextListener {
+
+    private lateinit var adapter: SearchAdapter
+    private lateinit var mUserRef: DatabaseReference
+//    private lateinit var options : FirebaseRecyclerAdapter<User,SearchAdapter.ViewHolder>
     override val TAG: String =this ::class.simpleName.toString()
     override fun getViewBinding(): FragmentSearchBinding =
         FragmentSearchBinding.inflate(layoutInflater)
 
     override fun setUp() {
         (activity as MainActivity).showBottomNavigationView()
-
+        val options = FirebaseRecyclerOptions.Builder<User>()
+            .setQuery(mUserRef , User::class.java).build()
+        adapter = SearchAdapter(options,requireContext())
+        binding.recyclerViewSearch.adapter = adapter
+        binding.recyclerViewSearch.layoutManager = LinearLayoutManager(requireContext())
+        mUserRef =FirebaseDatabase.getInstance().getReference("users")
     }
 
     override fun onQueryTextSubmit(query: String?): Boolean {
