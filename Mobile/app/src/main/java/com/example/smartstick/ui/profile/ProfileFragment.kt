a513972df24a093e1b93898913b15cc47b89453a
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-05-01 17:46:36 +0200

    add update relative number & current location

--- Mobile/app/src/main/java/com/example/smartstick/ui/profile/ProfileFragment.kt
+++ Mobile/app/src/main/java/com/example/smartstick/ui/profile/ProfileFragment.kt
@@ -1,5 +1,9 @@
 package com.example.smartstick.ui.profile
 
+import android.content.ContentValues
+import android.content.Context
+import android.location.Geocoder
+import android.util.Log
 import android.widget.Toast
 import com.example.smartstick.MainActivity
 import com.example.smartstick.data.base.BaseFragment
@@ -7,6 +11,7 @@ import com.example.smartstick.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import java.io.IOException
 
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
@@ -14,6 +19,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     private var mUserRef: DatabaseReference? = null
     private lateinit var mAuth: FirebaseAuth
     private var mUser: FirebaseUser? = null
+    var relativeNumber = ""
 
     override fun getViewBinding(): FragmentProfileBinding =
         FragmentProfileBinding.inflate(layoutInflater)
@@ -24,16 +30,26 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
         mUser = mAuth.currentUser
         mUserRef = FirebaseDatabase.getInstance().getReference("users")
         loadAccountData()
+        addCallBacks()
+    }
+    private fun addCallBacks(){
+        binding.btnSaveData.setOnClickListener {
+            updateRelativeNumber()
+            getMyHomeLocation()
+        }
     }
 
     private fun loadAccountData() {
-        mUserRef!!.child(mUser?.uid ?: "").addValueEventListener(object : ValueEventListener {
+        mUserRef!!.child(mUser!!.uid).addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 if (dataSnapshot.exists()) {
+                    relativeNumber =dataSnapshot.child("Relative Number").value.toString()
                     val email = dataSnapshot.child("email").value.toString()
                     val password = dataSnapshot.child("password").value.toString()
                     binding.textViewEmail.text = email
                     binding.editTextPassword.editText?.setText(password)
+                    binding.edittextPhone.setText(relativeNumber)
+
                 } else {
                     Toast.makeText(requireContext(), "Data Not Exist", Toast.LENGTH_LONG).show()
                 }
@@ -43,6 +59,57 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
 
             }
         })
+    }
+     private fun updateRelativeNumber(){
+         val num  =binding.edittextPhone.text.toString()
+         mUserRef!!.child(mUser!!.uid).child("Relative Number")
+             .setValue(num).addOnCompleteListener { task ->
+                 if (task.isSuccessful) {
+                     binding.edittextPhone.setText(num)
+                     Toast.makeText(requireContext()," You add your relative number  ",Toast.LENGTH_LONG).show()
+
+                 } else {
+                     // Error updating data
+                     val message = task.exception?.message ?: "Unknown error"
+                     Toast.makeText(requireContext(),"Erroe$message",Toast.LENGTH_LONG).show()
+                 }
+             }
+
+     }
+
+    private fun getMyHomeLocation(){
+        mUserRef!!.child(mUser!!.uid).child("location")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val locationString = dataSnapshot.value as String?
+                    val locationArray = locationString?.split(", ")
+                    val latitude = locationArray?.get(0)?.toDoubleOrNull()
+                    val longitude = locationArray?.get(1)?.toDoubleOrNull()
+
+                    if (latitude != null && longitude != null) {
+                       val address= getAddressLocation(requireContext(),latitude,longitude)
+                        binding.editTextLocation.setText(address)
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Log.e(ContentValues.TAG, "Error getting location data", databaseError.toException())
+                }
+            })
+    }
+    fun getAddressLocation(context: Context, latitude: Double, longitude: Double): String {
+        val geocoder = Geocoder(context)
+        try {
+            val addressList = geocoder.getFromLocation(latitude, longitude, 1)
+            if (addressList != null && addressList.isNotEmpty()) {
+                return addressList[0].getAddressLine(0) ?: ""
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+        }
+        return "City not found"
     }
 
 
