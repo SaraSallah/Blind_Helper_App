5e2b58430b4eed75f4b2c8d8365b0b0fbe2bc3e3
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-05-02 01:11:57 +0300

    get last update location with time

--- Mobile/app/src/main/java/com/example/smartstick/ui/tracking/MapsActivity.kt
+++ Mobile/app/src/main/java/com/example/smartstick/ui/tracking/MapsActivity.kt
@@ -15,6 +15,10 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.log
 
 class MapsActivity : AppCompatActivity() {
 
@@ -55,10 +59,18 @@ class MapsActivity : AppCompatActivity() {
                     val locationArray = locationString?.split(", ")
                     val latitude = locationArray?.get(0)?.toDoubleOrNull()
                     val longitude = locationArray?.get(1)?.toDoubleOrNull()
+                    val time = if ((locationArray?.size ?: 0) >= 3) locationArray?.get(2)?.toLongOrNull() else null
 
-                    if (latitude != null && longitude != null) {
+
+                    if (latitude != null && longitude != null && time != null ) {
+                        val currentDate = Date(time)
+                        val formatter = SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.getDefault())
+                        val formattedDate = formatter.format(currentDate)
+                        Log.e("Map",formattedDate.toString())
                         showFriendLocationOnMap(latitude, longitude)
+
                     }
+
                 }
 
                 override fun onCancelled(databaseError: DatabaseError) {
