120163408af018804f6463340a160c63f2ac5bd1
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-05-02 03:00:05 +0300

    create HolderAdapter

--- Mobile/app/src/main/java/com/example/smartstick/ui/home/HolderAdapter.kt
+++ Mobile/app/src/main/java/com/example/smartstick/ui/home/HolderAdapter.kt
@@ -7,20 +7,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.smartstick.R
 import com.example.smartstick.data.User
 import com.example.smartstick.databinding.SingleViewFindFriendBinding
-import com.example.smartstick.ui.search.SearchAdapter
-import com.firebase.ui.database.FirebaseRecyclerAdapter
-import com.firebase.ui.database.FirebaseRecyclerOptions
 
 class HolderAdapter (
     private val listener: HolderAdapter.UserInteractionListener,
-    private val relativeFriends: List<User>
+    private val relativeFriends: List<User>,
+    private val keys: List<String>
 ) :
     RecyclerView.Adapter<HolderAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.single_view_find_friend, parent, false)
-        return HolderAdapter.ViewHolder(view)
+        return ViewHolder(view)
     }
 
     override fun getItemCount() = relativeFriends.size
@@ -28,7 +26,7 @@ class HolderAdapter (
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val friend = relativeFriends[position]
         holder.binding.userEmail.text = friend.email
-        holder.binding.root.setOnClickListener { listener.onClickUser(friend(position).key.toString()) }
+        holder.binding.root.setOnClickListener { listener.onClickUser(keys[position]) }
     }
 
     interface UserInteractionListener {
