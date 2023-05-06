bccb7b527a7089ec24636ac77e4925eab97d4444
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-05-07 01:16:00 +0300

    handle space

--- Mobile/app/src/main/java/com/example/smartstick/ui/home/HolderFragment.kt
+++ Mobile/app/src/main/java/com/example/smartstick/ui/home/HolderFragment.kt
@@ -3,21 +3,11 @@ package com.example.smartstick.ui.home
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import com.example.smartstick.MainActivity
-import com.example.smartstick.R
 import com.example.smartstick.data.base.BaseFragment
 import com.example.smartstick.databinding.FragmentHolderBinding
-import com.example.smartstick.databinding.FragmentHomeBinding
 import com.example.smartstick.ui.tracking.LocationManager
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
 
 class HolderFragment : BaseFragment<FragmentHolderBinding>(){
     override val TAG: String = this::class.simpleName.toString()
--- Mobile/app/src/main/java/com/example/smartstick/ui/home/HomeFragment.kt
+++ Mobile/app/src/main/java/com/example/smartstick/ui/home/HomeFragment.kt
@@ -1,19 +1,14 @@
 package com.example.smartstick.ui.home
 
-import android.Manifest
 import android.content.ContentValues
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.util.Log
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smartstick.MainActivity
 import com.example.smartstick.data.User
 import com.example.smartstick.data.base.BaseFragment
 import com.example.smartstick.databinding.FragmentHomeBinding
 import com.example.smartstick.ui.search.SearchAdapter
-import com.example.smartstick.ui.tracking.LocationManager
 import com.example.smartstick.ui.tracking.MapsActivity
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.google.firebase.auth.FirebaseAuth
