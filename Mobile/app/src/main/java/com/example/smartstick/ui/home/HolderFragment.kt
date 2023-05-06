38bbc1bb879c62c126fca964270881dd68d27971
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-05-06 16:13:23 +0300

    add location services to holderFragment

--- Mobile/app/src/main/java/com/example/smartstick/ui/home/HolderFragment.kt
+++ Mobile/app/src/main/java/com/example/smartstick/ui/home/HolderFragment.kt
@@ -1,15 +1,21 @@
 package com.example.smartstick.ui.home
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.smartstick.MainActivity
 import com.example.smartstick.R
 import com.example.smartstick.data.base.BaseFragment
 import com.example.smartstick.databinding.FragmentHolderBinding
 import com.example.smartstick.databinding.FragmentHomeBinding
+import com.example.smartstick.ui.tracking.LocationManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
@@ -21,12 +27,55 @@ class HolderFragment : BaseFragment<FragmentHolderBinding>(){
 
 
     override fun setUp() {
-        (activity as MainActivity).showBottomNavigationView()
-//        startLocationService()
-//        friendRef = FirebaseDatabase.getInstance().getReference("Friends")
-//        mAuth = FirebaseAuth.getInstance()
-//        mUser = mAuth.currentUser!!
-//        getAllFriendsID()
+        startLocationService()
+    }
+
+    private fun startLocationService() {
+        if (isLocationPermissionGranted()) {
+            val intent = Intent(requireContext(), LocationManager::class.java)
+            requireContext().startService(intent)
+        } else {
+            requestLocationPermissions()
+        }
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationService()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Location permission required to use this feature",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 100
     }
 
 }
